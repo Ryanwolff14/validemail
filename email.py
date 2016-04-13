2a2483901430d7235e7af7b8c1b9ede030202b01
Ryanwolff14 <ryan.wolff14@outlook.com>
2016-04-13 15:17:10 -0500

    Added files via upload
    final

--- email.py
+++ email.py
@@ -1,7 +1,9 @@
+import re
+
 def validemail(): 
     email= userInput = input("please enter an email: ");
-    validemail= (r"[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+.[a-zA-Z]",email)
-    if validemail:
+    match= re.search(r'[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+.[a-zA-Z]',email)
+    if match:
         print("Valid Email")
     else:
         print("Invalid Email")
